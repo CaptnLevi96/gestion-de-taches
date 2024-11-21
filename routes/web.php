35726cf66134bb25e188767a6f96cfe4ba6f1c6d
Levi Loseke <157930732+CaptnLevi96@users.noreply.github.com>
2024-11-21 17:59:03 -0500

    Routes nommer

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Auth;
 
-// Rediriger vers les tâches si connecté, sinon vers login
 Route::get('/', function () {
     if (Auth::check()) {
         return redirect()->route('tasks.index');
     }
     return redirect()->route('login');
-});
+})->name('root');
 
 Auth::routes();
 
@@ -19,5 +18,11 @@ Route::get('/home', function() {
 })->name('home');
 
 Route::middleware(['auth'])->group(function () {
-    Route::resource('tasks', TaskController::class);
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
